80177676bd47b7e1df581f1697b530523a43bb14
Malo DAMIEN <elfamoso07@protonmail.com>
2022-07-16 17:06:30 +0200

    setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,25 +1,11 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
-import auth from '@/api/store'
-import App from '@/App.vue'
-import router from './router' 
-import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-import vuetify from './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
 
-const store = createStore({
-        modules: {
-          auth
-        }
-      })
-      
-      createApp(App)
-        .use(BootstrapVue3)
-        .use(router)
-        .use(store)
-        .use(vuetify)
-        .use(VueConfetti)
-        .component('font-awesome-icon', FontAwesomeIcon)
-        .mount('#app')
-      
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
