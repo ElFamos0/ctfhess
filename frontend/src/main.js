72eaf48a9ddbbd31f027c96082a5c31bc473e5e3
Malo DAMIEN <elfamoso07@protonmail.com>
2022-07-27 18:40:43 +0200

    [+] Oauth google

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import gAuthPlugin from 'vue3-google-oauth2'
+const app = createApp(App)
 
-createApp(App).mount('#app')
+let gauthClientId = '130553832427-1oi6h98j0qkhlg3931b7bmcq65qho5jb.apps.googleusercontent.com';
+app.use(gAuthPlugin, {
+        clientId: gauthClientId,
+        scope: 'email',
+        prompt: 'consent',
+})
+app.mount('#app')
