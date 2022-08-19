406703c46ceef2ad7fcc57270c1f81f75c562f0f
Malo DAMIEN <malo.damien@telecomnancy.eu>
2022-08-19 23:20:00 +0200

    [+] lol

--- frontend/src/apis/axios-api.js
+++ frontend/src/apis/axios-api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//let base_backend_url = `http://${process.env.VUE_APP_BACKEND_DOMAIN}:${process.env.VUE_APP_BACKEND_PORT}/api`;
-let base_backend_url = `http://localhost:8000/api`;
+let base_backend_url = `http://${process.env.VUE_APP_BACKEND_DOMAIN}:${process.env.VUE_APP_BACKEND_PORT}/api`;
 const getAPI = axios.create({
   baseURL: base_backend_url,
   headers: {
