fd88db08a67f3d26952d78fc0e7b2bc677bfedd1
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2022-08-21 19:45:55 +0200

    [+] J'ai oublié axios..

--- frontend/src/apis/axios-api.js
+++ frontend/src/apis/axios-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let base_backend_url = `http://${process.env.VUE_APP_BACKEND_DOMAIN}:${process.env.VUE_APP_BACKEND_PORT}/api`;
+let base_backend_url = `${process.env.VUE_APP_BACKEND_URI}/api`;
 axios.defaults.withCredentials = true;
 const getAPI = axios.create({
   baseURL: base_backend_url,
