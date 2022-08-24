35606fa8a93a5e56eea7e2b1e5c58b7439a044a4
Malo DAMIEN <malo.damien@telecomnancy.eu>
2022-08-24 17:55:12 +0200

    [+] Le cors de ses morts

--- backend/server/server.go
+++ backend/server/server.go
@@ -20,7 +20,7 @@ func InitServer() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8080", "https://chall.wordchamp.info"},
+		AllowOrigins:     []string{"http://localhost:8080", "https://ctfhess.com"},
 		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
